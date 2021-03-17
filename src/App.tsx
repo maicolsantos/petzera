ed6ca85ae5ceff7eacaa32da143f787df9fe9581
Maicol Santos <maicolsantos@sas.com.br>
2021-03-17 19:57:46 -0300

    Ajustar import para o arquivo index

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'

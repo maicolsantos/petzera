f96c376077d2f700db0fcdbc1d68c0fb454e5065
Maicol Santos <maicolsantos@sas.com.br>
2021-03-17 20:26:46 -0300

    Adicionar ícones e criar botão para adicionar pet

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { PlusCircle } from 'react-feather'
 
-import { ContainerGradient, Title } from './styles'
+import {
+  ContainerGradient,
+  Title,
+  Card,
+  CardTitle,
+} from './styles'
 
 const Home = () => (
   <ContainerGradient>
     <Title>PetZera</Title>
+    <Card>
+      <CardTitle>Adicionar Pet</CardTitle>
+      <PlusCircle color="#0093E9" />
+    </Card>
   </ContainerGradient>
 )
 
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -14,4 +14,21 @@ export const Title = styled.Text`
   color: white;
   font-size: 24px;
   font-weight: bold;
+  margin-bottom: 24px;
+`
+
+export const Card = styled.TouchableOpacity`
+  background: white;
+  padding: 16px 24px;
+  border-radius: 100px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+export const CardTitle = styled.Text`
+  color: #0093E9;
+  font-size: 16px;
+  font-weight: bold;
+  margin-right: 16px;
 `
